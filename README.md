c5caac89ab809b76b8a20d484c18101ab2ffa3d1
John T <John@Thomas-household.com>
2020-07-30 21:13:12 -0500

    completed notes app

---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Note_Taker
+##### Project Description
+This appliation is an express note taker. Its functionality is a a basi notes app that allows you to add, delete, and edit notes.The notes include a title with a description below it
+
+
+
+## What I used 
+* express
+* path
+* fs
+*  get and post request
+
+
+## Heroku link
+*  https://app-notes123.herokuapp.com/notes
+
+## Contacts
+
+* Name: John Thomas
+* email: John@thomas-household.com
---
+++ server.js
@@ -0,0 +1,106 @@
+// Bringing in our dependencies
+var express = require("express");
+var path = require("path");
+var fs = require("fs")
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+// display notes.html when at /notes
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname,'public', "notes.html"));
+});
+
+
+// API GET Request
+// Returns contents of notes array
+app.get("/api/notes", function(req, res) {
+  fs.readFile('db/db.json', "utf8", function(err, data){
+    if (err){
+      return console.log(err)
+    }
+    else {
+      var notes = JSON.parse(data);
+      res.json(notes);
+    }
+  });
+});
+
+// API Post 
+// Adds new note from user to the array
+app.post("/api/notes", function(req, res) {
+  var postit = req.body;
+  var notes = [];
+
+  fs.readFile('db/db.json', "utf8", function(err, data){
+    if(err){
+      return console.log(err)
+    }
+    notes = JSON.parse(data);
+
+    var ids = [];
+    for(var i = 0; i < notes.length; i++){
+      ids.push(notes[i].id)
+    };
+
+    var highId = Math.max.apply(null, ids);
+
+    postit.id = highId + 1;
+
+    notes.push(postit);
+
+    fs.writeFile('db/db.json', JSON.stringify(notes), "UTF-8", function (err){
+      if (err){
+        return console.log(err);
+      }
+      res.json(postit);
+    })
+  })
+});
+
+// API Delete 
+// Deletes a note from the array
+app.delete('/api/notes/:id', function(req, res){
+  var idDelete = req.params.id;
+  var notes = [];
+
+  fs.readFile('db/db.json', "utf8", function(err, data){
+    if(err){
+      return console.log(err);
+    }
+    
+      notes = JSON.parse(data);
+
+      for (var i = 0; i < notes.length; i++){
+        if(notes[i].id == idDelete) {
+          notes.splice(i,1);
+        }
+      }
+      fs.writeFile('db/db.json', JSON.stringify(notes), "UTF-8", function (err){
+        if(err){
+          return console.log(err);
+        }
+        res.json(notes);
+      })
+  })
+})
+
+
+// Default to home page if no matching route is found
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname,'public', "index.html"));
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App is running on http://localhost:" + PORT);
+  });
+  
+  https://app-notes123.herokuapp.com/notes
